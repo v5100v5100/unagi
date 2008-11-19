b1b7c7dc941ec01d73d261fbbe0ebcbcacc57112
sato_tiff <sato_tiff@24ea1065-a21e-4ca1-99c9-f5125deb0858>
2008-11-19 21:48:42 +0000

    client 0.4.0 release

--- client/tag/0.4.0/paralellport.h
+++
@@ -1,9 +0,0 @@
-#ifndef _PARALELLPORT_H
-#define _PARALELLPORT_H
-
-void reader_init(void);
-void cpu_read(long address, long length, u8 *data);
-void ppu_read(long address, long length, u8 *data);
-void cpu_write(long address, long data);
-void ppu_write(long address, long data);
-#endif
