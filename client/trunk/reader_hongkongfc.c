842ceaf10f1208055035e203fcec247f5174875e
naruko <naruko@24ea1065-a21e-4ca1-99c9-f5125deb0858>
2009-01-13 11:21:38 +0000

    cpu_flash_write, ppu_write のデータバスの出力タイミングを修正

--- client/trunk/reader_hongkongfc.c
+++ client/trunk/reader_hongkongfc.c
@@ -216,15 +216,16 @@ static void hk_ppu_write(long address, long data)
 	//cpu rom を止めたアドレスを渡す
 	address_set((address & ADDRESS_MASK_A0toA12) | ADDRESS_MASK_A15);
 	data_port_set(c, data); 
-	c = bit_clear(c, BITNUM_WRITEDATA_OUTPUT);
 	//CS down
 	c = bit_clear(c, BITNUM_PPU_SELECT);
 	data_port_latch(DATA_SELECT_CONTROL, c);
 	//WE down
+	c = bit_clear(c, BITNUM_WRITEDATA_OUTPUT);
 	c = bit_clear(c, BITNUM_PPU_RW);
 	data_port_latch(DATA_SELECT_CONTROL, c);
 	//WE up
 	c = bit_set(c, BITNUM_PPU_RW);
+	c = bit_set(c, BITNUM_WRITEDATA_OUTPUT);
 	data_port_latch(DATA_SELECT_CONTROL, c);
 	//CS up
 	data_port_latch(DATA_SELECT_CONTROL, BUS_CONTROL_BUS_STANDBY);
@@ -259,10 +260,10 @@ W49F002
 hongkong 系は address と /ROMCS が同じバイトで、 /CS 制御にすると
 hongkong データ破壊+アドレス不安定になるので、/WE 制御にしないと動かない。
 */
-	c = bit_clear(c, BITNUM_WRITEDATA_OUTPUT);
 	//CS down
 	cpu_romcs_set(address & ADDRESS_MASK_A0toA14);
 	//WE down
+	c = bit_clear(c, BITNUM_WRITEDATA_OUTPUT);
 	c = bit_clear(c, BITNUM_CPU_RW);
 //	c = bit_clear(c, BITNUM_CPU_M2);
 	data_port_latch(DATA_SELECT_CONTROL, c);
