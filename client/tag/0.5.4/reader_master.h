38053e358d8ac06f995710395c6f57c36a06cce9
naruko <naruko@24ea1065-a21e-4ca1-99c9-f5125deb0858>
2009-01-27 05:02:43 +0000

    client 0.5.4 release

---
+++ client/tag/0.5.4/reader_master.h
@@ -0,0 +1,20 @@
+#ifndef _READER_MASTER_H_
+#define _READER_MASTER_H_
+#include "type.h"
+//C++ の Class もどきを C で実装している感が増してきた...
+struct reader_driver{
+	int (*open_or_close)(int oc);
+	void (*init)(void);
+	void (*cpu_read)(long address, long length, u8 *data);
+	void (*ppu_read)(long address, long length, u8 *data);
+	void (*cpu_6502_write)(long address, long data, long wait_msec);
+	void (*cpu_flash_write)(long address, long data);
+	void (*ppu_write)(long address, long data);
+	const char *name;
+};
+int paralellport_open_or_close(int oc);
+const struct reader_driver *reader_driver_get(const char *name);
+enum{
+	READER_OPEN, READER_CLOSE
+};
+#endif
