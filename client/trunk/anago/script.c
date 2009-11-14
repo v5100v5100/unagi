51513cb8c193b8866edc82cd20f5f81b6fb815f9
naruko <naruko@24ea1065-a21e-4ca1-99c9-f5125deb0858>
2009-11-14 18:48:08 +0000

    progress 表示を実装

--- client/trunk/anago/script.c
+++ client/trunk/anago/script.c
@@ -7,12 +7,13 @@
 #include "reader_master.h"
 #include "squirrel_wrap.h"
 #include "flash_device.h"
+#include "progress.h"
 #include "script.h"
 
 struct anago_driver{
 	struct anago_flash_order{
 		bool command_change;
-		long address, length;
+		long address, length, program_count, program_offset;
 		long c000x, c2aaa, c5555;
 		struct memory *const memory;
 		struct flash_device *const device;
@@ -129,8 +130,8 @@ static SQInteger program_regist(HSQUIRRELVM v, const char *name, struct anago_fl
 		t->command_change = false;
 	}
 	
-	printf("programming %s ROM area 0x%06x...\n", name, t->memory->offset);
-	fflush(stdout);
+/*	printf("programming %s ROM area 0x%06x...\n", name, t->memory->offset);
+	fflush(stdout);*/
 	return sq_suspendvm(v);
 }
 static void program_execute(struct anago_flash_order *t)
@@ -142,8 +143,9 @@ static void program_execute(struct anago_flash_order *t)
 	t->length -= w;
 	t->memory->offset += w;
 	t->memory->offset &= t->memory->size - 1;
+	t->program_offset += w;
 }
-static SQInteger cpu_program(HSQUIRRELVM v)
+static SQInteger cpu_program_memory(HSQUIRRELVM v)
 {
 	struct anago_driver *d;
 	SQRESULT r =  qr_userpointer_get(v, (SQUserPointer *) &d);
@@ -152,7 +154,7 @@ static SQInteger cpu_program(HSQUIRRELVM v)
 	}
 	return program_regist(v, "program", &d->order_cpu);
 }
-static SQInteger ppu_program(HSQUIRRELVM v)
+static SQInteger ppu_program_memory(HSQUIRRELVM v)
 {
 	struct anago_driver *d;
 	SQRESULT r =  qr_userpointer_get(v, (SQUserPointer *) &d);
@@ -162,6 +164,17 @@ static SQInteger ppu_program(HSQUIRRELVM v)
 	return program_regist(v, "charcter", &d->order_ppu);
 }
 
+static long erase_timer_get(struct anago_flash_order *t)
+{
+	if(
+		(t->memory->transtype != TRANSTYPE_EMPTY) && 
+		(t->device->erase_require == true)
+	){
+		return t->device->erase_wait;
+	}else{
+		return 0;
+	}
+}
 static SQInteger erase_wait(HSQUIRRELVM v)
 {
 	struct anago_driver *d;
@@ -169,11 +182,20 @@ static SQInteger erase_wait(HSQUIRRELVM v)
 	if(SQ_FAILED(r)){
 		return r;
 	}
-	uint8_t s[2];
-	do{
-		Sleep(2);
-		d->flash_status(s);
-	}while((s[0] != 0) && (s[1] != 0));
+	if(0){
+		long timer_wait = erase_timer_get(&d->order_cpu);
+		long timer_ppu = erase_timer_get(&d->order_ppu);
+		if(timer_wait < timer_ppu){
+			timer_wait = timer_ppu;
+		}
+		Sleep(timer_wait);
+	}else{
+		uint8_t s[2];
+		do{
+			Sleep(2);
+			d->flash_status(s);
+		}while((s[0] != 0) && (s[1] != 0));
+	}
 	return 0;
 }
 
@@ -196,8 +218,11 @@ static SQInteger program_main(HSQUIRRELVM v)
 	}
 	SQInteger state_cpu = sq_getvmstate(co_cpu);
 	SQInteger state_ppu = sq_getvmstate(co_ppu);
-	while(state_cpu != SQ_VMSTATE_IDLE || state_ppu != SQ_VMSTATE_IDLE){
+
+	progress_init();
+	while((state_cpu != SQ_VMSTATE_IDLE) || (state_ppu != SQ_VMSTATE_IDLE)){
 		uint8_t s[2];
+		bool console_update = false;
 		Sleep(2);
 		d->flash_status(s);
 		if(state_cpu != SQ_VMSTATE_IDLE && s[0] == 0){
@@ -206,6 +231,7 @@ static SQInteger program_main(HSQUIRRELVM v)
 				state_cpu = sq_getvmstate(co_cpu);
 			}else{
 				program_execute(&d->order_cpu);
+				console_update = true;
 			}
 		}
 		if(state_ppu != SQ_VMSTATE_IDLE && s[1] == 0){
@@ -214,16 +240,91 @@ static SQInteger program_main(HSQUIRRELVM v)
 				state_ppu = sq_getvmstate(co_ppu);
 			}else{
 				program_execute(&d->order_ppu);
+				console_update = true;
 			}
+		}
+		if(console_update == true){
+			progress_draw(d->order_cpu.program_offset, d->order_cpu.program_count, d->order_ppu.program_offset, d->order_ppu.program_count);
 		}
 	}
 	return 0;
+}
+
+static SQInteger script_nop(HSQUIRRELVM v)
+{
+	return 0;
+}
+
+static SQInteger program_count(HSQUIRRELVM v, struct anago_flash_order *t)
+{
+	SQRESULT r = qr_argument_get(v, 2, &t->address, &t->length);
+	if(SQ_FAILED(r)){
+		return r;
+	}
+	t->program_count += t->length;
+	return 0;
+}
+static SQInteger cpu_program_count(HSQUIRRELVM v)
+{
+	struct anago_driver *d;
+	SQRESULT r =  qr_userpointer_get(v, (SQUserPointer *) &d);
+	if(SQ_FAILED(r)){
+		return r;
+	}
+	return program_count(v, &d->order_cpu);
+}
+
+static SQInteger ppu_program_count(HSQUIRRELVM v)
+{
+	struct anago_driver *d;
+	SQRESULT r =  qr_userpointer_get(v, (SQUserPointer *) &d);
+	if(SQ_FAILED(r)){
+		return r;
+	}
+	return program_count(v, &d->order_ppu);
+}
+
+static bool script_testrun(struct config *c, struct anago_driver *d)
+{
+	static const char *functionname[] = {
+		"cpu_write", "cpu_erase", "cpu_command",
+		"ppu_write", "ppu_erase", "ppu_command",
+		"erase_wait", "program_main"
+	};
+	HSQUIRRELVM v = qr_open();
+	int i;
+	for(i = 0; i < sizeof(functionname)/sizeof(char *); i++){
+		qr_function_register_global(v, functionname[i], script_nop);
+	}
+	qr_function_register_global(v, "cpu_program", cpu_program_count);
+	qr_function_register_global(v, "ppu_program", ppu_program_count);
+
+	bool ret = true;
+	if(SQ_FAILED(sqstd_dofile(v, _SC("flashcore.nut"), SQFalse, SQTrue))){
+		printf("flash core script error\n");
+		ret = false;
+	}else if(SQ_FAILED(sqstd_dofile(v, _SC(c->script), SQFalse, SQTrue))){
+		printf("%s open error\n", c->script);
+		ret = false;
+	}else{
+		qr_call(
+			v, "program", (SQUserPointer) d, true, 
+			5, c->rom.mappernum, 
+			d->order_cpu.memory->transtype, d->order_cpu.memory->size, 
+			d->order_ppu.memory->transtype, d->order_ppu.memory->size
+		);
+	}
+
+	qr_close(v);
+	return ret;
 }
 void script_execute(struct config *c)
 {
 	struct anago_driver d = {
 		.order_cpu = {
 			.command_change = true,
+			.program_count = 0,
+			.program_offset = 0,
 			.device = &c->flash_cpu,
 			.memory = &c->rom.cpu_rom,
 			.config = c->reader->cpu_flash_config,
@@ -235,6 +336,8 @@ void script_execute(struct config *c)
 		},
 		.order_ppu = {
 			.command_change = true,
+			.program_count = 0,
+			.program_offset = 0,
 			.device = &c->flash_ppu,
 			.memory = &c->rom.ppu_rom,
 			.config = c->reader->ppu_flash_config,
@@ -246,14 +349,17 @@ void script_execute(struct config *c)
 		},
 		.flash_status = c->reader->flash_status
 	};
+	if(script_testrun(c, &d) == false){
+		return;
+	}
 	
 	HSQUIRRELVM v = qr_open(); 
 	qr_function_register_global(v, "cpu_write", cpu_write);
 	qr_function_register_global(v, "cpu_erase", cpu_erase);
-	qr_function_register_global(v, "cpu_program", cpu_program);
+	qr_function_register_global(v, "cpu_program", cpu_program_memory);
 	qr_function_register_global(v, "cpu_command", cpu_command);
 	qr_function_register_global(v, "ppu_erase", ppu_erase);
-	qr_function_register_global(v, "ppu_program", ppu_program);
+	qr_function_register_global(v, "ppu_program", ppu_program_memory);
 	qr_function_register_global(v, "ppu_command", ppu_command);
 	qr_function_register_global(v, "program_main", program_main);
 	qr_function_register_global(v, "erase_wait", erase_wait);
