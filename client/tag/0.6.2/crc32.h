e157b43f3fb39561cc697d00b1dfd8b2375d0190
naruko <naruko@24ea1065-a21e-4ca1-99c9-f5125deb0858>
2010-08-01 06:55:09 +0000

    removed wrong tag

--- client/tag/0.6.2/crc32.h
+++
@@ -1,4 +0,0 @@
-#ifndef _CRC32_H_
-#define _CRC32_H_
-uint32_t crc32_get(const uint8_t *buf, int len);
-#endif
--- client/tag/0.6.2/reader_kazzo.h
+++
@@ -1,4 +0,0 @@
-#ifndef _READER_KAZZO_H_
-#define _READER_KAZZO_H_
-const struct reader_driver DRIVER_KAZZO;
-#endif
